73bb454789e5a8d049b64aa1c360f8b316670e23
gracemugeche <gracemugeche@gmail.com>
2025-06-28 19:04:44 +0300

    add the routes of every page

---
+++ client/src/routes/AppRoutes.tsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Cart from "../pages/Cart";
+import Checkout from "../pages/Checkout";
+import Meal from "../pages/Meals";
+import Groceries from "../pages/Groceries";
+import Drink from "../pages/Drinks";
+import Signup from "../pages/Signup";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signUp" element={<Signup />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/meals" element={<Meal />} />
+      <Route path="/groceries" element={<Groceries/>} />
+      <Route path="/drinks" element={<Drink/>} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
