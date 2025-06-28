40a3a59f9a06a4e455382c81a5ed5e3501c0e833
gracemugeche <gracemugeche@gmail.com>
2025-06-28 18:59:49 +0300

    feat: add a home page

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import AboutSection from "../components/AboutSection";
+import CategoriesSection from "../components/CategoriesSection";
+import HeroSection from "../components/HeroSection";
+
+const Home = () => {
+  return (
+    <main>
+      <HeroSection />
+      <CategoriesSection />
+      <AboutSection/>
+    </main>
+  );
+};
+
+export default Home;
