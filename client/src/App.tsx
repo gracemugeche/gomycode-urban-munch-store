2832625db85e3653bdf747295e7c63f73131993a
gracemugeche <gracemugeche@gmail.com>
2025-06-28 18:55:10 +0300

    feat:update the app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,25 @@
-
+// src/App.tsx
+import { useLocation } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+// import AppRoutes from "./routes/AppRoutes";
 
 export default function App() {
+  const location = useLocation();
+
+  // Hide navbar and footer on login/signup pages
+  const hideLayoutRoutes = ["/login", "/signup","/cart" ,"/checkout"];
+  const shouldHideLayout = hideLayoutRoutes.includes(location.pathname);
+
   return (
-    <div className="text-blue-500">
-      hey
+    <div className="flex flex-col min-h-screen">
+      {!shouldHideLayout && <Navbar />}
+      
+      <main className="flex-grow">
+        {/* <AppRoutes /> */}
+      </main>
+      
+      {!shouldHideLayout && <Footer />}
     </div>
-  )
+  );
 }
