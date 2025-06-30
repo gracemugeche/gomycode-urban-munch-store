f456b5af883e1c7bdc5c583f4f45f73618c5799b
gracemugeche <gracemugeche@gmail.com>
2025-06-30 16:04:22 +0300

    updated the types folder

---
+++ server/src/types/cart.ts
@@ -0,0 +1,4 @@
+export interface ICartItem {
+  product: string;
+  quantity: number;
+}
---
+++ server/src/types/order.ts
@@ -0,0 +1,26 @@
+export interface IOrderItem {
+  product: string;
+  name: string;
+  quantity: number;
+  price: number;
+  image: string;
+}
+
+export interface IOrder {
+  _id: string;
+  user: string;
+  orderItems: IOrderItem[];
+  deliveryAddress: {
+    street: string;
+    city: string;
+    phone: string;
+  };
+  paymentMethod: 'Cash on Delivery' | 'Stripe'| 'm-pesa';
+  totalPrice: number;
+  isPaid: boolean;
+  paidAt?: Date;
+  isDelivered: boolean;
+  deliveredAt?: Date;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
