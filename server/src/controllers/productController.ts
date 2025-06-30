0834b2d95f9907e82f4fee859b14f4a35acd3ece
gracemugeche <gracemugeche@gmail.com>
2025-06-30 10:44:34 +0300

    feat: add Product model and controller with getAllProducts function

---
+++ server/src/controllers/productController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import Product from "../models/productModels";
+
+export const getAllProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching products" });
+  }
+};
