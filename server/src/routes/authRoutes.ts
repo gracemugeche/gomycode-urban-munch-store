712d1759200221532fd521f835b5261c14f86006
gracemugeche <gracemugeche@gmail.com>
2025-06-30 17:00:44 +0300

    feat:add the validaterequest middleware in authroutes and cartroutes

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,14 +1,29 @@
 import express from "express";
+import { body } from "express-validator";
 import {
   loginUser,
   registerUser,
   getMe,
 } from "../controllers/authControllers";
 import { protect } from "../middlewares/authMiddleware";
+import { validateRequest } from "../middlewares/validateRequest";
 
 const router = express.Router();
 
-router.post("/register", registerUser);
+// User registration with validation
+router.post(
+  "/register",
+  [
+    body("name").notEmpty().withMessage("Name is required"),
+    body("email").isEmail().withMessage("Valid email is required"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters"),
+  ],
+  validateRequest,
+  registerUser
+);
+
 router.post("/login", loginUser);
 router.get("/me", protect, getMe);
 
--- server/src/routes/cartRoutes.ts
+++ server/src/routes/cartRoutes.ts
@@ -1,11 +1,30 @@
 import express from 'express';
+import { body } from 'express-validator';
 import { getCart, addToCart, removeFromCart } from '../controllers/cartController';
 import { protect } from '../middlewares/authMiddleware';
+import { validateRequest } from '../middlewares/validateRequest';
 
 const router = express.Router();
 
+// Get the user's cart
 router.get('/', protect, getCart);
-router.post('/', protect, addToCart);
+
+// Add an item to the cart (with validation)
+router.post(
+  '/',
+  protect,
+  [
+    body('product').notEmpty().withMessage('Product ID is required'),
+    body('name').notEmpty().withMessage('Product name is required'),
+    body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than 0'),
+    body('quantity').isInt({ min: 1 }).withMessage('Quantity must be at least 1'),
+    body('image').notEmpty().withMessage('Image URL is required'),
+  ],
+  validateRequest,
+  addToCart
+);
+
+// Remove item from cart
 router.delete('/:productId', protect, removeFromCart);
 
 export default router;
