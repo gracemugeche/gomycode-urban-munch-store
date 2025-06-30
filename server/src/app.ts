b8d7f8a7e621d28a703e461e6b9728b5fd2913ed
gracemugeche <gracemugeche@gmail.com>
2025-06-30 10:50:14 +0300

    feat: integrate productRoutes into Express app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+
+import productRoutes from './routes/productRoutes';
 // import authRoutes from './routes/authRoutes'; // you’ll create this soon
 // import errorHandler from './middlewares/errorHandler'; // optional for now
-//create app
+
+// Create Express app
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
 // Routes
+app.use('/api/products', productRoutes);
 // app.use('/api/auth', authRoutes); // placeholder route
 
 // Error handler
